df142ec40c4ec438d60e49237c691c9b21905cc7
Daniel <daniel.albert.liu@gmail.com>
2026-05-24 08:34:10 -0700

    remove translator struct

--- l3/src/translate.rs
+++ l3/src/translate.rs
@@ -1,25 +1,6 @@
 use l2;
 use l3::*;
 
-// TODO: not sure about this
-#[derive(Debug)]
-pub struct Translator;
-
-impl Translator {
-    pub fn symbol_id(id: SymbolId) -> l2::SymbolId {
-        l2::SymbolId(id.0)
-    }
-
-    pub fn value(val: &Value) -> l2::Value {
-        match val {
-            Value::Number(num) => l2::Value::Number(*num),
-            Value::Label(label) => l2::Value::Label(translate_symbol_id(*label)),
-            Value::Function(callee) => l2::Value::Function(translate_symbol_id(*callee)),
-            Value::Variable(var) => l2::Value::Variable(translate_symbol_id(*var)),
-        }
-    }
-}
-
 pub fn translate_symbol_id(id: SymbolId) -> l2::SymbolId {
     l2::SymbolId(id.0)
 }
