35f7a97c21849b18bfbc896b7f678e963c0cd356
Daniel <daniel.albert.liu@gmail.com>
2026-05-30 17:27:10 -0700

    clean up code, probably faster too

--- l3/src/mangling.rs
+++ l3/src/mangling.rs
@@ -3,28 +3,9 @@ use std::collections::HashMap;
 use l3::*;
 
 pub fn mangle_labels(prog: &mut Program) -> (String, u32) {
-    let mut prefix = prog
-        .functions
-        .iter()
-        .flat_map(|func| &func.basic_blocks)
-        .flat_map(|block| &block.instructions)
-        .fold(None, |prefix, inst| match inst {
-            Instruction::Label(label)
-            | Instruction::Branch(label)
-            | Instruction::BranchCondition { label, .. } => {
-                let label = prog.interner.resolve(label.0);
-                match prefix {
-                    None => Some(label.clone()),
-                    Some(prefix) if label.len() > prefix.len() => Some(label.clone()),
-                    _ => prefix,
-                }
-            }
-            _ => prefix,
-        })
-        .unwrap_or("".to_owned());
-    prefix.push_str("_global_");
-
+    let prefix = longest_label(prog).to_owned();
     let mut suffix = 0;
+
     for func in &mut prog.functions {
         let mut label_to_mangled = HashMap::new();
 
@@ -36,7 +17,10 @@ pub fn mangle_labels(prog: &mut Program) -> (String, u32) {
                     | Instruction::BranchCondition { label, .. } => {
                         let is_new = !label_to_mangled.contains_key(label);
                         *label = *label_to_mangled.entry(*label).or_insert_with(|| {
-                            SymbolId(prog.interner.intern(format!("{}{}", prefix, suffix)))
+                            SymbolId(
+                                prog.interner
+                                    .intern(format!("{}_global_{}", prefix, suffix)),
+                            )
                         });
                         suffix += is_new as u32;
                     }
@@ -47,4 +31,25 @@ pub fn mangle_labels(prog: &mut Program) -> (String, u32) {
     }
 
     (prefix, suffix)
+}
+
+fn longest_label(prog: &Program) -> &str {
+    prog.functions
+        .iter()
+        .flat_map(|func| &func.basic_blocks)
+        .flat_map(|block| &block.instructions)
+        .fold(None, |longest, inst| match inst {
+            Instruction::Label(label)
+            | Instruction::Branch(label)
+            | Instruction::BranchCondition { label, .. } => {
+                let label = prog.interner.resolve(label.0);
+                match longest {
+                    None => Some(label),
+                    Some(longest) if label.len() > longest.len() => Some(label),
+                    _ => longest,
+                }
+            }
+            _ => longest,
+        })
+        .map_or("", |longest| longest)
 }
